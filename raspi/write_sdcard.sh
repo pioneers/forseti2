89c4f0df201b19407b5bbe0a375e4ddb2178143d
Andrew J. Chen <chenjandrew@gmail.com>
2014-02-26 15:38:08 -0800

    changes to write_sdcard

--- raspi/write_sdcard.sh
+++ raspi/write_sdcard.sh
@@ -5,4 +5,4 @@
 # writes an sd card image to the sd card.
 # via http://elinux.org/RPi_Easy_SD_Card_Setup#Using_the_Linux_command_line
 
-sudo dd bs=4M if=~/2012-12-16-wheezy-raspbian.img of=/dev/mmcblk0
+sudo dd bs=4M if=sdimage.img of=/dev/mmcblk0
