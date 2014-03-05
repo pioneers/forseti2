c6038622d0f100d2e29d7cdda1dfd2eebea5ea95
Andrew J. Chen <chenjandrew@gmail.com>
2014-03-05 07:29:32 -0800

    das blinkin lights

--- src/firmata_test.py
+++ src/firmata_test.py
@@ -4,14 +4,26 @@ import time
 from pyfirmata import ArduinoMega, util
 import pyfirmata
 
-board = ArduinoMega('/dev/ttyUSB1')
+board = ArduinoMega('/dev/ttyUSB0')
 board.digital[2].mode = pyfirmata.SERVO
+stime=.05
 while True:
-    for i in (22, 24, 26):
-        board.digital[i].write(True)
-    board.digital[2].write(0)
-    time.sleep(1)
-    for i in (22, 24, 26):
+    for i in (26, 24, 22):
         board.digital[i].write(False)
-    board.digital[2].write(180)
-    time.sleep(1)
+        time.sleep(stime)
+        board.digital[i].write(True)
+        time.sleep(stime)
+
+    # for i in (24):
+    board.digital[24].write(False)
+    time.sleep(stime)
+    board.digital[24].write(True)
+    time.sleep(stime)
+    
+
+    # board.digital[2].write(0)
+    #time.sleep(.05)
+    #for i in (22, 24, 26):
+
+    # board.digital[2].write(180)
+   
