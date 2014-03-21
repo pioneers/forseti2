ad829091e248d6cc18f17c3223d96b6eb2a237ca
Andrew J. Chen <chenjandrew@gmail.com>
2014-03-20 23:36:24 -0700

    tested forest2/trunk

--- src/firmata_test.py
+++ src/firmata_test.py
@@ -4,26 +4,33 @@ import time
 from pyfirmata import ArduinoMega, util
 import pyfirmata
 
-board = ArduinoMega('/dev/ttyUSB0')
-board.digital[2].mode = pyfirmata.SERVO
-stime=.05
+board = ArduinoMega('/dev/ttyACM0')
+
+RED=0
+ORANGE=1
+GREEN=2
+SERVO=3
+
+combinations = []
+combinations.append((22, 24, 26, 2))
+combinations.append((28, 30, 32, 3))
+combinations.append((34, 36, 38, 4))
+combinations.append((40, 42, 44, 5))
+combinations.append((46, 48, 50, 6))
+combinations.append((31, 33, 35, 7))
+combinations.append((37, 39, 41, 8))
+combinations.append((43, 45, 47, 9))
+
+stime=.5
+for combo in combinations:
+    board.digital[combo[SERVO]].mode = pyfirmata.SERVO
 while True:
-    for i in (26, 24, 22):
-        board.digital[i].write(False)
+    for i in range(3):
+        for combo in combinations:
+            board.digital[combo[i]].write(False)
+            board.digital[combo[SERVO]].write(0)
         time.sleep(stime)
-        board.digital[i].write(True)
+        for combo in combinations:
+            board.digital[combo[i]].write(True)
+            board.digital[combo[SERVO]].write(180)
         time.sleep(stime)
-
-    # for i in (24):
-    board.digital[24].write(False)
-    time.sleep(stime)
-    board.digital[24].write(True)
-    time.sleep(stime)
-    
-
-    # board.digital[2].write(0)
-    #time.sleep(.05)
-    #for i in (22, 24, 26):
-
-    # board.digital[2].write(180)
-   
