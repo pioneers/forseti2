4f69d8e8e3de4279d4dbbdba1a659fcf400fa8d5
Allen Li <allen@allen.li>
2014-04-11 04:20:48 -0700

    implement first end-to-end

--- judge/judge_flask.py
+++ judge/judge_flask.py
@@ -1,19 +1,43 @@
 from flask import *
-app = Flask(__name__)
+import threading
+import lcm
+import forseti2 as fs2
+import sys
+sys.path.append('../src')
+import settings
 
-greeting = 'Hello World'
-name = "Andrew"
+app = Flask(__name__)
 
 @app.route('/')
 def serve_console():
     return render_template('judge_console.html')
 
-@app.route('/api/v1/<method>', methods=['GET', 'POST'])
-def api(method):
-    if request.method == 'POST':
-        return greeting
-    else:
+stored_time = 0
+@app.route('/api/v1/game_time')
+def game_time():
+    return str(stored_time)
+
+def handle_xbox(channel, data):
+    global stored_time
+    msg = fs2.xbox_joystick_state.decode(data)
+    stored_time = msg.buttons[0]
+
+def main():
+    global lc
+    lc = lcm.LCM(settings.LCM_URI)
+    subscription = lc.subscribe("xbox/state/default/0", handle_xbox)
+    try:
+        while True:
+            lc.handle()
+    except KeyboardInterrupt:
         pass
 
-if __name__ == '__main__':
+def run_flask_app():
     app.run(debug = True)
+
+if __name__ == '__main__':
+    t = threading.Thread(target = main)
+    t.daemon = True
+    t.start()
+
+    run_flask_app()
--- judge/static/judge_console.js
+++ judge/static/judge_console.js
@@ -1,10 +1,17 @@
 function populateGameClock() {
-	return 1 + 1;
+	$.get('/api/v1/game_time', {}, function(data) {
+		$('#game-clock').text(data);
+	});
+
 }
 
-// main function
+function main_loop() {
+	d = new Date();
+	time = d.toLocaleTimeString();
+	$('#wall-clock').text(time);
+	populateGameClock();
+}
+
 $( document ).ready(function() {
-	while (1) {
-		populateGameClock()
-	}
+	window.setInterval(main_loop, 100);
 });
