d9778f3920667043c95fc7270c212ecd0c97b469
Nikita Kitaev <nikitakit@gmail.com>
2014-04-12 20:37:48 -0700

    Penalize alliances for over-possession of the bonus ball

--- src/score_server.py
+++ src/score_server.py
@@ -27,8 +27,12 @@ class ScoreServer:
     def __init__(self):
         self.lc = lcm.LCM(settings.LCM_URI)
         self.sub = self.lc.subscribe("score/delta", self.delta_handler)
+        self.time_sub = self.lc.subscribe("Timer/Time", self.time_handler)
         self.seq = util.LCMSequence(self.lc, forseti2.score_state, "score/state")
         self.seq.debug = False
+
+        self.current_time = 0
+        self.bonus_penalty_time = 0 # Time when bonus penalty will be assessed
 
         self.reset_scores()
 
@@ -46,6 +50,26 @@ class ScoreServer:
 
     def handle(self):
         self.lc.handle()
+
+    def time_handler(self, channel, data):
+        msg = forseti2.Time.decode(data)
+        self.current_time = msg.game_time_so_far
+
+        if self.current_time >= self.bonus_penalty_time:
+            if self.state["bonus_possession"] == forseti2.score_delta.BLUE:
+                self.state["blue_penalty"] += settings.PENALTY_BONUS_TIMER
+                print "Blue alliance penalized for over-possession of the bonus ball"
+            elif self.state["bonus_possession"] == forseti2.score_delta.GOLD:
+                self.state["gold_penalty"] += settings.PENALTY_BONUS_TIMER
+                print "Gold alliance penalized for over-possession of the bonus ball"
+            else:
+                return
+
+            self.bonus_penalty_time = self.current_time + settings.BONUS_TIMER_SECONDS
+
+            self.print_score()
+            self.seq.publish(**self.tabulate_state())
+
 
     def delta_handler(self, channel, data):
         msg = forseti2.score_delta.decode(data)
@@ -71,6 +95,7 @@ class ScoreServer:
         # If bonus points are included as part of a bonus possession change delta,
         # do not count those points if the delta turns out to be invalid
         count_bonus_points = False
+        reset_bonus_timer = False
         if msg.bonus_possession == forseti2.score_delta.UNCHANGED:
             count_bonus_points = True
         elif msg.bonus_possession == forseti2.score_delta.NEUTRAL:
@@ -82,17 +107,23 @@ class ScoreServer:
             elif self.state["bonus_possession"] == forseti2.score_delta.BLUE:
                 self.state["bonus_possession"] = forseti2.score_delta.GOLD
                 count_bonus_points = True
+                reset_bonus_timer = True
             elif self.state["bonus_possession"] == forseti2.score_delta.GOLD:
                 self.state["bonus_possession"] = forseti2.score_delta.BLUE
                 count_bonus_points = True
+                reset_bonus_timer = True
         elif msg.bonus_possession == self.state["bonus_possession"]:
             print "WARNING: ignoring attempt to reassign bonus ball to the team already in possession"
         else:
             self.state["bonus_possession"] = msg.bonus_possession
             count_bonus_points = True
+            reset_bonus_timer = True
 
         if count_bonus_points:
             self.state["bonus_points"] += msg.bonus_points
+
+        if reset_bonus_timer:
+            self.bonus_penalty_time = self.current_time + settings.BONUS_TIMER_SECONDS
 
         self.print_score()
         self.seq.publish(**self.tabulate_state())
