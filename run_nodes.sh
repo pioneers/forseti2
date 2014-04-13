b332714c9bb0349f8d68ef305893eb6ab07ba95a
Nikita Kitaev <nikitakit@gmail.com>
2014-04-13 00:08:46 -0700

    Script to start all field control nodes in a tmux window

---
+++ run_nodes.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+if command -v tmux; then
+    :
+else
+    echo "ERROR: tmux is not installed"
+    exit 1
+fi
+
+if command -v gem; then
+    :
+else
+    echo "ERROR: Ruby 'gem' command is not installed"
+    exit 1
+fi
+
+if gem list teamocil -i | grep false; then
+    echo "ERROR: teamocil not installed"
+    echo "Please run 'sudo gem install teamocil'"
+    exit 1
+fi
+
+export TEAMOCIL_PATH="$PWD"
+
+echo "This script must be run in a tmux session"
+echo "If there is an error, type 'tmux' and then re-run the script"
+teamocil nodes
