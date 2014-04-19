5bf0a0065469148f8faf53e656038634405df2ba
Felix Li <felixli@pioneers.berkeley.edu>
2014-04-19 05:35:54 -0700

    Adjusted forest driver and firmata to reflect pin changes on forest3, rainy

--- src/firmata_test.py
+++ src/firmata_test.py
@@ -4,7 +4,7 @@ import time
 from pyfirmata import ArduinoMega, util
 import pyfirmata
 
-board = ArduinoMega('/dev/ttyACM0')
+board = ArduinoMega('/dev/ttyUSB0')
 
 RED=0
 ORANGE=1
@@ -12,14 +12,14 @@ GREEN=2
 SERVO=3
 
 combinations = []
-combinations.append((22, 24, 26, 2))
-combinations.append((28, 30, 32, 3))
-combinations.append((34, 36, 38, 4))
-combinations.append((40, 42, 44, 5))
-combinations.append((46, 48, 50, 6))
-combinations.append((31, 33, 35, 7))
-combinations.append((37, 39, 41, 8))
-combinations.append((43, 45, 47, 9))
+combinations.append((22, 23, 24, 2))
+combinations.append((25, 26, 27, 3))
+combinations.append((28, 29, 30, 4))
+combinations.append((31, 32, 33, 5))
+combinations.append((34, 35, 36, 6))
+combinations.append((37, 38, 39, 7))
+combinations.append((40, 41, 42, 8))
+combinations.append((43, 44, 45, 9))
 
 stime=.5
 for combo in combinations:
