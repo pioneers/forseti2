17ee9cb3225f1d46c5cb895d84ae17ee7006f7ed
karthik-shanmugam <karthiks@berkeley.edu>
2016-03-12 16:23:24 -0800

    refactor lighthouse logic

--- src/field_control.py
+++ src/field_control.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import argparse
 import forseti2
 import configurator
 import json
@@ -10,41 +11,57 @@ import os
 import settings
 import util
 import LCMNode
+import grizzly
+import pyfirmata
 
 Node = LCMNode.Node
 LCMNode = LCMNode.LCMNode
-# changes here go to network_monitor, lighthouse timer 
-# lcm channel names are "[Class-name]/[.lcm file name]"
+
 class Button(Node):
 
-    def __init__(self, index): #TODO: give channel_name
+    def __init__(self, index, arduino_path=None, use_arduino=False):
+        self.use_arduino = use_arduino
         self.send_channel = "Button%d/Button" % index
         self.button = forseti2.Button()
-        self.button.pressed = False
+        self.pressed = False
         self.lc = lcm.LCM(settings.LCM_URI)
-        self.start_thread() #runs overridden loop function from LCMNode.py
+        if self.use_arduino:
+            self.board = pyfirmata.Arduino(arduino_path)
+            self.pin = self.board.get_pin('d:4:i')
+            self.it = pyfirmata.util.Iterator(self.board)
+            self.it.start()
+            self.pin.enable_reporting()
+        self.start_thread()
+        self.start_time = time.time()
 
-    #TODO: check the button
-    def checkButton(self):
-        return self.button.pressed
+
+    def check_button(self):
+        if self.use_arduino:
+            self.update(self.pin.read())
 
     def _loop(self):
         while True:
-            time.sleep(0.3)
-            self.lc.publish(self.send_channel, self.button.encode())
-            #self.button.pressed = not self.button.pressed
+            time.sleep(0.05)
+            self.check_button()
+            if time.time() - self.start_time > .3:
+                self.start_time = time.time()
+                #print(self.pressed)
+                self.button.pressed = self.pressed
+                self.clear()
+                self.lc.publish(self.send_channel, self.button.encode())
 
-    def press(self):
-        self.button.pressed = True
+    def clear(self):
+        self.pressed = False
 
-    def depress(self):
-        self.button.pressed = False
+    def update(self, pressed):
+        if pressed:
+            self.pressed = True
+
 
 
 class Motor(LCMNode):
 
-    #TODO: if button pressed and time since button press < 5 seconds, motor should be on, should only be on again if timer ends
-    def __init__(self, index): #TODO: give send_channels
+    def __init__(self, index, grizzly_addr=None, use_grizzly=False):
         self.index = index
         self.send_channel = "Motor%d/Motor" % index
         self.receive_channel = "LighthouseTimer/LighthouseTime"
@@ -56,26 +73,36 @@ class Motor(LCMNode):
         self.start_time = time.time()
         self.start_thread()
         self.start_thread(target=self.run())
+        if use_grizzly:
+            self.grizzly = grizzly.Grizzly(grizzly_addr)
+            self.grizzly.set_mode(ControlMode.NO_PID, DriveMode.DRIVE_BRAKE)
+            self.grizzly.set_target(0)
 
     def handle_control(self, channel, data):
         msg = forseti2.LighthouseTime.decode(data)
-        print('Received command', msg.counter) #TODO: check that the timer has stopped before starting motor
-        if msg.activated and msg.counter != self.counter and msg.button_index == self.index:
-            self.counter = msg.counter
-            self.activate()
-        else:
-            self.deactivate()
+        # turns out there's only one shooter for now, so we can bypass this check
+        if msg.button_index == self.index or True:
+            if msg.enabled and not msg.available: 
+                if msg.counter != self.counter:
+                    self.counter = msg.counter
+                    self.activate()
+            else:
+                self.deactivate()
 
-    def activate():
+    def activate(self):
         if not self.motor.activated:
             self.motor.activated = True
             self.start_time = time.time()
-            print("Motor Activated")
+            if use_grizzly:
+                self.grizzly.set_target(100)
+            print(time.strftime('Motor Activated at %l:%M:%S %p'))
 
-    def deactivate():
+    def deactivate(self):
         if self.motor.activated:
             self.motor.activated = False
-            print("Motor Deactivated")
+            if use_grizzly:
+                self.grizzly.set_target(0)
+            print(time.strftime('Motor Deactivated at %l:%M:%S %p'))
 
     def check(self, timeout=5):
         if self.motor.activated and time.time() - self.start_time >= 5:
@@ -89,53 +116,17 @@ class Motor(LCMNode):
             time.sleep(.03)
 
 def main():
-    buttons = [Button(i) for i in range(2)] #automatically starts looping
-    motor = [Motor(i)  for i in range(2)]
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-b', '--button', action='append', nargs='+', metavar=('index', 'arduino_path'), help="create a button with optional arduino location")
+    parser.add_argument('-m', '--motor', action='append', nargs='+', metavar=('index', 'grizzly_id'), help="create a motor with optional grizzly id")
+    args = parser.parse_args()
+    if args.button:
+        buttons = [Button(int(button[0]), button[1], True) if len(button) > 1 else Button(int(button[0])) for button in args.button]
+    if args.motor:
+        motors = [Motor(int(motor[0]), motor[1], True) if len(motor) > 1 else Motor(int(motor[0])) for motor in args.motor]
+    #button0 = Button(0, "/dev/tty.usbmodem1421", True)
+    #buttons = [Button(i) for i in range(2)] #automatically starts looping
+    #motors = [Motor(0, 0, True), Motor(1, 1, False)]
 
 if __name__ == '__main__':
     main()
-"""import lcm 
-import forseti2
-import settings
-import time
-#CREATE TWO OBJECTS FOR BUTTON AND MOTOR EXTEND LCMNODE 
-#PUT CHANNEL NAME AS PROPERTY 
-#PUT INFINITE LOOP IN A RUN METHOD
-lc = lcm.LCM(settings.LCM_URI)
-#creating two packets for different presses
-pressed_button = LCM.Button()
-pressed_button.pressed = True
-pressed_button.encode()
-
-unpressed_button = LCM.Button()
-unpressed_button.pressed = False
-unpressed_button.encode()
-
-#creating two packets for different motor states
-activated_motor = LCM.Motor()
-activated_motor.activated = True
-activated_motor.encode()
-
-unactivated_motor = LCM.Motor()
-unactivated_motor.activated = False
-unactivated_motor.encode()
-
-button_state = True
-motor_state = True
-pressed = True
-activate = True
-
-def handle_motor(channel, data):
-    pass
-
-lc.subscribe('Motor', self.handle_motor)
-while True:
-    if button_state:
-        lc.publish("Button/Press", pressed_button if pressed else unpressed_button)
-        button_state = not button_state
-        pressed = not pressed
-    else: 
-        lc.publish("Motor", activated_motor if activate else unactivated_motor)
-        motor_state = not motor_state
-        activate = not activate
-        """
--- src/lighthouse_timer.py
+++ src/lighthouse_timer.py
@@ -20,6 +20,7 @@ import random
 import os
 import settings
 import util
+import re
 import LCMNode
 
 Node = LCMNode.Node
@@ -57,8 +58,7 @@ class LighthouseTimer(LCMNode):
         self.timer = Timer()
         self.stage_name = "Setup"
         self.buttons = [False, False]
-        #self.lc.subscribe("Game_Button/Button", self.handle_control)
-        self.lc.subscribe("Game_Button[0-9]/Button", self.handle_control)
+        self.lc.subscribe(r"Button[0-9]/Button", self.handle_control)
         self.lc.subscribe("Timer/Time", self.handle_time)
         self.start_thread()
         self.counter = random.randint(0, 1000000)
@@ -69,49 +69,59 @@ class LighthouseTimer(LCMNode):
             msg = forseti2.LighthouseTime()
 
             if self.stage_name in ["Setup", "Paused", "End"]:
+                self.timer.pause()
+                msg.enabled = False
                 msg.available = False
+                msg.counter = self.counter
                 msg.time_left = 0
-                self.lc.publish('lighthouseTimer/LighthouseTime', msg.encode())
+                self.lc.publish('LighthouseTimer/LighthouseTime', msg.encode())
                 continue
                 
             if self.buttons[0]:
                 self.buttons[0] = False
                 if not self.timer.running:
+                    self.counter += 1
                     self.timer.start()
-                    msg.buttonIndex = 0
+                    msg.button_index = 0
 
             if self.buttons[1]:
                 self.buttons[1] = False
                 if not self.timer.running:
+                    self.counter += 1
                     self.timer.start()
-                    msg.buttonIndex = 1
+                    msg.button_index = 1
+            self.clear()
 
             lighthouse_time = self.timer.time()
             time_left = 10*1000 - lighthouse_time*1000
             if time_left <= 0:
                 self.timer.pause()
-
+            
+            msg.enabled = True
+            msg.counter = self.counter
             if not self.timer.running:
                 msg.time_left = 10*1000
-                self.counter += 1
-                msg.counter = self.counter
                 msg.available = True
                 self.lc.publish('LighthouseTimer/LighthouseTime', msg.encode())
             else:
                 msg.time_left = time_left
-                msg.is_lighthouse_on = False
+                msg.available = False
                 self.lc.publish('LighthouseTimer/LighthouseTime', msg.encode())
-         
+
+    def press(self, index, pressed):
+        if pressed:
+            self.buttons[index] = True
+
+    def clear(self):
+        self.buttons = [False, False]
 
     def handle_control(self, channel, data):
         msg = forseti2.Button.decode(data)
-        #msg = forseti2.Button1.decode(data)
-        #TODO: Parse channel into two to pass into the arrays, 
-        # using if statements 
-        self.buttons[0] = msg.button.pressed
-        self.buttons[1] = msg.button.pressed
+        match = re.match(r"Button([0-9])/Button", channel)
+        if match:
+            self.press(int(match.groups()[0]), msg.pressed)
 
-    def handle_time(self,channel,data):
+    def handle_time(self, channel, data):
         msg = forseti2.Time.decode(data)
         self.stage_name = msg.stage_name
 
