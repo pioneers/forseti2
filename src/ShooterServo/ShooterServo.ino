1ec766e298207a4b7ff2a1647380af49c0a3cac9
karthik-shanmugam <karthik@karthiks.me>
2016-04-22 01:01:50 -0700

    add lighthouse sounds

--- src/ShooterServo/ShooterServo.ino
+++ src/ShooterServo/ShooterServo.ino
@@ -5,14 +5,16 @@ void setup() {
   s.attach(9);
   pinMode(11, OUTPUT);
   pinMode(12, OUTPUT);
+  digitalWrite(11, HIGH);
+  digitalWrite(12, HIGH);
 }
 
 void loop() {
-  while (!Serial.available()) {
-  }
-  int servo = Serial.read();
-  Serial.write(servo);
-  s.write(servo);
+//  while (!Serial.available()) {
+//  }
+//  int servo = Serial.read();
+//  Serial.write(servo);
+//  s.write(servo);
   
   while (!Serial.available()) {
   }
@@ -20,9 +22,11 @@ void loop() {
   if (motors > '0') {
     digitalWrite(11, LOW);
     digitalWrite(12, LOW);
+    s.write(180);
   } else {
     digitalWrite(11, HIGH);
     digitalWrite(12, HIGH);
+    s.write(0);
   }
   Serial.write(motors);
  
