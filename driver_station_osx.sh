3bab4e66260a2f020c6a45ac53b445facfed73fd
karthik-shanmugam <karthik@karthiks.me>
2016-04-24 08:30:06 -0700

    Update driver_station_osx.sh

--- driver_station_osx.sh
+++ driver_station_osx.sh
@@ -2,12 +2,12 @@ cd
 rm scan_wifi.sh*
 wget https://raw.githubusercontent.com/pioneers/forseti2/2016/scan_wifi.sh
 sudo chmod 777 scan_wifi.sh
-rm dawn-linux-x64.tar.gz*
+rm dawn-darwin-x64.tar.gz*
 wget https://dl.dropboxusercontent.com/s/5f0idms5spxxr11/dawn-darwin-x64.tar.gz
-sudo tar -xvzf dawn-linux-x64.tar.gz
-rm dawn-linux-x64.tar.gz*
+sudo tar -xvzf dawn-darwin-x64.tar.gz
+rm dawn-darwin-x64.tar.gz*
 sudo rm -rf /opt/driver_station
-sudo mv dawn-linux-x64 /opt/driver_station
+sudo mv dawn-darwin-x64 /opt/driver_station
 sudo chmod -R 777 /opt/driver_station
 DEFAULT_FIELD_IP=192.168.0.101
 echo -n "Field IP (default $DEFAULT_FIELD_IP) ?"
